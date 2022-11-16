5c75ac430b15d28ef22f49bef618c00341281764
Geoff Phillips <phigeoff@amazon.de>
2022-11-16 16:50:47 +0100

    Add bash error handling, change to AWS Docker repo, fix Dockerfile

--- tools/container/start-fwe.sh
+++ tools/container/start-fwe.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-if [[ -z "$CAN_IF" ]]; then
+set -euo pipefail
+if [[ -z ${CAN_IF+x} ]]; then
     echo "Must provide CAN_IF in environment" 1>&2
     exit 1
 fi
 
-if [[ -z "$VEHICLE_NAME" ]]; then
+if [[ -z ${VEHICLE_NAME+x} ]]; then
     echo "Must provide VEHICLE_NAME in environment" 1>&2
     exit 1
 fi
 
-if [[ -z "$FW_ENDPOINT" ]]; then
+if [[ -z ${FW_ENDPOINT+x} ]]; then
     echo "Must provide FW_ENDPOINT in environment" 1>&2
     exit 1
 fi
@@ -29,13 +30,13 @@ while : ; do
 done
 
 mkdir -p /var/aws-iot-fleetwise
-mkdir -p /etc/fwe
+mkdir -p /etc/aws-iot-fleetwise
 /usr/bin/configure-fwe.sh \
         --input-config-file /etc/static-config.json \
         --output-config-file /etc/aws-iot-fleetwise/config-0.json \
         --vehicle-name $VEHICLE_NAME \
         --endpoint-url $FW_ENDPOINT \
         --can-bus0 $CAN_IF 
-[[ $TRACE == "on" ]]  && sed -i 's/Info/Trace/' /etc/aws-iot-fleetwise/config-0.json 
+[[ -v TRACE ]] && [[ $TRACE == "on" ]] && sed -i 's/Info/Trace/' /etc/aws-iot-fleetwise/config-0.json 
 cat /etc/aws-iot-fleetwise/config-0.json
 /usr/bin/aws-iot-fleetwise-edge /etc/aws-iot-fleetwise/config-0.json
