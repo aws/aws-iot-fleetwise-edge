27dc8f8ca8e213418989a5b492c155245fbed151
Stu Cao <shuxucao@amazon.com>
2023-08-24 19:33:14 +0000

    debug package source issue

--- tools/install-deps-cross-arm64.sh
+++ tools/install-deps-cross-arm64.sh
@@ -44,6 +44,7 @@ sed -i "s/deb http/deb [arch=${ARCH}] http/g" /etc/apt/sources.list
 cp /etc/apt/sources.list /etc/apt/sources.list.d/arm64.list
 sed -i "s/deb \[arch=${ARCH}\] http/deb [arch=arm64] http/g" /etc/apt/sources.list.d/arm64.list
 sed -i -E "s#(archive|security).ubuntu.com/ubuntu#ports.ubuntu.com/ubuntu-ports#g" /etc/apt/sources.list.d/arm64.list
+cat /etc/apt/sources.list.d/arm64.list
 dpkg --add-architecture arm64
 apt update
 apt install -y \
