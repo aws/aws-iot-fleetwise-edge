2b5e4e7cc6850a4ba6305d13c8275136c7d41f28
Vasu Bansal <vasuban@amazon.com>
2024-09-04 05:35:11 +0000

    Added flag to ignore tests

--- tools/test-fwe.sh
+++ tools/test-fwe.sh
@@ -5,16 +5,23 @@
 set -eo pipefail
 
 WITH_ROS2_SUPPORT="false"
+IGNORE_TESTS=""
 
 parse_args() {
     while [ "$#" -gt 0 ]; do
         case $1 in
         --with-ros2-support)
             WITH_ROS2_SUPPORT="true"
+            shift
+            ;;
+        --extra-ctest-args)
+            IGNORE_TESTS=$2
+            shift
             ;;
         --help)
             echo "Usage: $0 [OPTION]"
             echo "  --with-ros2-support  Test with ROS2 support"
+            echo "  --extra-ctest-args Gives the tests to ignore"
             exit 0
             ;;
         esac
@@ -26,10 +33,16 @@ parse_args "$@"
 
 if ${WITH_ROS2_SUPPORT}; then
     source /opt/ros/galactic/setup.bash
-    # colcon hides the test output, so use tail in the background.
-    tail -F log/latest_test/iotfleetwise/stdout_stderr.log &
-    colcon test --return-code-on-test-failure
+    if [ -n "${IGNORE_TESTS}" ]; then
+        ctest -V ${IGNORE_TESTS}
+    else
+        ctest -V
+    fi
 else
     cd build
-    ctest -V
+    if [ -n "${IGNORE_TESTS}" ]; then
+        ctest -V ${IGNORE_TESTS}
+    else
+        ctest -V
+    fi
 fi
